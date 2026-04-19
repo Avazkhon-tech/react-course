946e70f39e22407362ef7dbd464424b4f1857bfa
avazxon.nazirov <avazxon.nazirov@uzinfocom.uz>
2026-04-19 12:48:08 +0500

    add: create OrdersPage component and update routing in App component

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,16 +1,18 @@
 import {Routes, Route} from 'react-router'
 import {HomePage} from './pages/HomePage.jsx';
 import {CheckoutPage} from "./pages/CheckoutPage.jsx";
+import {OrdersPage} from "./pages/OrdersPage.jsx";
 import './App.css'
 
 function App() {
 
-  return (
-      <Routes>
-        <Route index element={<HomePage/>} />
-        <Route path="/checkout" element={<CheckoutPage/>} />
-      </Routes>
-  )
+    return (
+        <Routes>
+            <Route index element={<HomePage/>}/>
+            <Route path="/checkout" element={<CheckoutPage/>}/>
+            <Route path="/orders" element={<OrdersPage/>}/>
+        </Routes>
+    )
 }
 
 export default App
